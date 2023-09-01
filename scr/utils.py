e74368c16f18cec1f2585c64c12dab26a6ce1152
f.neumann <53896206+f-neumann77@users.noreply.github.com>
2023-09-01 15:17:25 +0400

    Add files via upload

---
+++ scr/utils.py
@@ -0,0 +1,121 @@
+import numpy as np
+import scipy.io as sio
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+import torch
+from operator import truediv
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, cohen_kappa_score
+
+
+class TrainDS(torch.utils.data.Dataset):
+
+    def __init__(self, Xtrain, ytrain):
+
+        self.len = Xtrain.shape[0]
+        self.x_data = torch.FloatTensor(Xtrain)
+        self.y_data = torch.LongTensor(ytrain)
+
+    def __getitem__(self, index):
+
+        # Returns data and corresponding labels according to index
+        return self.x_data[index], self.y_data[index]
+    def __len__(self):
+
+        # Returns the number of file data
+        return self.len
+
+""" Testing dataset"""
+
+class TestDS(torch.utils.data.Dataset):
+
+    def __init__(self, Xtest, ytest):
+
+        self.len = Xtest.shape[0]
+        self.x_data = torch.FloatTensor(Xtest)
+        self.y_data = torch.LongTensor(ytest)
+
+    def __getitem__(self, index):
+
+        # Returns data and corresponding labels according to index
+        return self.x_data[index], self.y_data[index]
+
+    def __len__(self):
+
+        # Returns the number of file data
+        return self.len
+
+
+def loadData(hsi_path, hsi_key, gt_path, gt_key):
+    # Reading data
+    data = sio.loadmat(hsi_path)[hsi_key]
+    labels = sio.loadmat(gt_path)[gt_key]
+
+    return data, labels
+
+def applyPCA(X, numComponents):
+
+    newX = np.reshape(X, (-1, X.shape[2]))
+    pca = PCA(n_components=numComponents, whiten=True)
+    newX = pca.fit_transform(newX)
+    newX = np.reshape(newX, (X.shape[0], X.shape[1], numComponents))
+
+    return newX
+
+def padWithZeros(X, margin=2):
+
+    newX = np.zeros((X.shape[0] + 2 * margin, X.shape[1] + 2* margin, X.shape[2]))
+    x_offset = margin
+    y_offset = margin
+    newX[x_offset:X.shape[0] + x_offset, y_offset:X.shape[1] + y_offset, :] = X
+
+    return newX
+
+def createImageCubes(X, y, windowSize=5, removeZeroLabels = True):
+
+    # 给 X 做 padding
+    margin = int((windowSize - 1) / 2)
+    zeroPaddedX = padWithZeros(X, margin=margin)
+    # split patches
+    patchesData = np.zeros((X.shape[0] * X.shape[1], windowSize, windowSize, X.shape[2]))
+    patchesLabels = np.zeros((X.shape[0] * X.shape[1]))
+    patchIndex = 0
+    for r in range(margin, zeroPaddedX.shape[0] - margin):
+        for c in range(margin, zeroPaddedX.shape[1] - margin):
+            patch = zeroPaddedX[r - margin:r + margin + 1, c - margin:c + margin + 1]
+            patchesData[patchIndex, :, :, :] = patch
+            patchesLabels[patchIndex] = y[r-margin, c-margin]
+            patchIndex = patchIndex + 1
+    if removeZeroLabels:
+        patchesData = patchesData[patchesLabels>0,:,:,:]
+        patchesLabels = patchesLabels[patchesLabels>0]
+        patchesLabels -= 1
+
+    return patchesData, patchesLabels
+
+def splitTrainTestSet(X, y, testRatio, randomState=345):
+    X_train, X_test, y_train, y_test = train_test_split(X,
+                                                        y,
+                                                        test_size=testRatio,
+                                                        random_state=randomState,
+                                                        stratify=y)
+
+    return X_train, X_test, y_train, y_test
+
+def AA_andEachClassAccuracy(confusion_matrix):
+
+    list_diag = np.diag(confusion_matrix)
+    list_raw_sum = np.sum(confusion_matrix, axis=1)
+    each_acc = np.nan_to_num(truediv(list_diag, list_raw_sum))
+    average_acc = np.mean(each_acc)
+    return each_acc, average_acc
+
+def acc_reports(y_test, y_pred_test):
+
+    target_names = ['class 1', 'class 2', 'class 3']
+    classification = classification_report(y_test, y_pred_test, digits=4, target_names=target_names)
+    oa = accuracy_score(y_test, y_pred_test)
+    confusion = confusion_matrix(y_test, y_pred_test)
+    each_acc, aa = AA_andEachClassAccuracy(confusion)
+    kappa = cohen_kappa_score(y_test, y_pred_test)
+
+    return classification, oa*100, confusion, each_acc*100, aa*100, kappa*100
